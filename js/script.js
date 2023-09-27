9549f68d560eb4a2fedbb3c57ccb48e225a05e1e
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-09-27 16:29:31 +0200

    [Add]:details with wue

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('Vue ok', Vue)
+
+const root = new Vue({
+  el: '#root',
+  data: {
+      firstName: 'Andreea',
+      lastName: 'Ciobota',
+      age: '26',
+      birthPlace: 'Padova',
+      birthPlaceGif: 'https://media.tenor.com/skeQYeyTa5EAAAAC/zecchino-d-oro-le-luci.gif',
+  }
+})
